a1a40b4a1af2ad8bf54948e4dda64c492e97d094
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-21 11:42:02 +0200

    Updated app.py

--- crypto/app.py
+++ crypto/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask (__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/articles')
+def articles():
+    return render_templates('articles.html', articles = Articles)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
