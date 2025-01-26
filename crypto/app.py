25e0bae809bcbf6f268b69ac9a83e2a88b2b70dc
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-27 00:39:50 +0200

    app.py

--- crypto/app.py
+++ crypto/app.py
@@ -282,7 +282,6 @@ def handle_realtime_data():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True) 
     port = int(os.environ.get("PORT", 4000)) 
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
      
