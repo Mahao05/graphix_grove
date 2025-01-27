bd184d8735d1f68e57e8cb3e0f7543ead985d52d
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-27 14:48:35 +0200

    Update app.py

--- crypto/app.py
+++ crypto/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+from app import app
 
 app = Flask(__name__)
 
@@ -7,4 +9,5 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
