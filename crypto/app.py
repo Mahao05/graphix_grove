68fe9d4ef14c1f0b2fe93cd656cdd331691182d4
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-27 15:53:53 +0200

    Update app.py

--- crypto/app.py
+++ crypto/app.py
@@ -9,6 +9,5 @@ run_with_ngrok(app)
 def hello_world():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    webbrowser.open_new('http://127.0.0.1:5000/') 
+if __name__ == "__main__": 
     app.run()
