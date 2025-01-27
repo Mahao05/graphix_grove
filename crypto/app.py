9f9c78071a85817276a7d73953f9fb0e89ef0625
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-27 05:16:24 +0200

    Update app.py

--- crypto/app.py
+++ crypto/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request, jsonify
-from forms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flask_socketio import SocketIO
