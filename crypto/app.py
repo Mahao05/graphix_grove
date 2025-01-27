670b9199a25388b7ea8046e66ea0dc269abbed25
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-01-27 16:46:50 +0200

    Update app.py

--- crypto/app.py
+++ crypto/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-import webbrowser
-from flask_ngok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__": 
-    app.run()
+    app.run(debug=True)
